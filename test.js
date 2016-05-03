9918fa5d0638eac5ef1600f8056470143bc8a955
Guilherme Vieira <super.driver.512@gmail.com>
2016-05-03 03:18:30 -0300

    Use done on all test promises to avoid Q's exception swallowing behavior.

--- test.js
+++ test.js
@@ -17,40 +17,40 @@
 let Q = require('q');
 let Qh = require('.');
 
-Qh.console.log('1 - Hello, world:', 'Hello, world');
-Qh.console.log('2 - [ \'Hello, world\' ]:', [Q.when('Hello, world')]);
+Qh.console.log('1 - Hello, world:', 'Hello, world').done();
+Qh.console.log('2 - [ \'Hello, world\' ]:', [Q.when('Hello, world')]).done();
 
 // ---
 
-Qh.console.log('3 - 1,2,3:', Qh.callMethod('toString', [1, 2, 3]));
+Qh.console.log('3 - 1,2,3:', Qh.callMethod('toString', [1, 2, 3])).done();
 
 let xs = Q.when([1, 2, 3]);
 
-Qh.callMethod('push', xs, 4, Q.when(5));
+Qh.callMethod('push', xs, 4, Q.when(5)).done();
 
-Qh.console.log('4 - [ 1, 2, 3, 4, 5 ]:', xs);
+Qh.console.log('4 - [ 1, 2, 3, 4, 5 ]:', xs).done();
 
 // ---
 
-Qh.console.log('5 - true:', Qh.and());
+Qh.console.log('5 - true:', Qh.and()).done();
 
-Qh.console.log('6 - true:', Qh.and(1, 2, 3));
+Qh.console.log('6 - true:', Qh.and(1, 2, 3)).done();
 
-Qh.console.log('7 - false:', Qh.and(1, 0, 2, 3));
+Qh.console.log('7 - false:', Qh.and(1, 0, 2, 3)).done();
 
 // --
 
-Qh.console.log('8 - true:', Qh.or());
+Qh.console.log('8 - true:', Qh.or()).done();
 
-Qh.console.log('9 - true:', Qh.or(1, 2, 3));
+Qh.console.log('9 - true:', Qh.or(1, 2, 3)).done();
 
-Qh.console.log('10 - true:', Qh.or(0, 0, 0, 0, 0, 1, 0, 0));
+Qh.console.log('10 - true:', Qh.or(0, 0, 0, 0, 0, 1, 0, 0)).done();
 
-Qh.console.log('11 - false:', Qh.or(0, 0, 0));
+Qh.console.log('11 - false:', Qh.or(0, 0, 0)).done();
 
 // ---
 
-Qh.console.log('12 - 24:', Qh.mult(2, Q.when(3), 4));
+Qh.console.log('12 - 24:', Qh.mult(2, Q.when(3), 4)).done();
 
 // ---
 
@@ -58,10 +58,10 @@ Qh.ifElse(
     Q.when(true),
         () => console.log('13 - ok'),
         () => console.log('13 - error')
-);
+).done();
 
 Qh.ifElse(
     Q.when(false),
         () => console.log('13 - error'),
         () => console.log('13 - ok')
-);
+).done();
